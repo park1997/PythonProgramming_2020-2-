cfdb5dcb4a3de11b088d88417ba93c69560af4ea
park1997 <iambyunghyun97@gmail.com>
2020-11-09 15:04:54 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,9 +1,10 @@
-from functools import reduce
-def matrix_mul(*args):
-    result=reduce(lambda x,y : [[sum(a*b for a,b in zip(i,j))for j in zip(*y)]for i in x], args)
-    return result
+def scalar_vector_product(scalar_v,vector_list):
+    return [scalar_v*i for i in vector_list]
 
-matrix_x=[[1,2,3],[4,5,6]]
-matrix_y=[[1,2],[3,4],[5,6]]
-matrix_z=[[-1,0],[0,-1]]
-print(matrix_mul(matrix_x,matrix_y,matrix_z))
+
+
+scalar_v=int(input())
+vector_list=list(map(int,input().split()))
+
+
+print(scalar_vector_product(scalar_v,vector_list))
