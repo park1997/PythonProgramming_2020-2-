371557970e81fee2ea9c9e4d5a57a2118963ea52
park1997 <iambyunghyun97@gmail.com>
2020-11-10 14:53:00 +0900

    Update CH9_5.py
    i updated num.5
    
    the revised one is more better than before one

--- 2016112568_ParkByeogHyeon/CH9/CH9_5.py
+++ 2016112568_ParkByeogHyeon/CH9/CH9_5.py
@@ -1,9 +1,7 @@
 #3개 이상의 행렬의 곱이 가능한지 채점의 수월함을 위해 주석처리 해놓았습니다.
+from functools import reduce
 def matrix_mul(*z):
-    result=[[[sum(a*b for a,b in zip(r,c)) for c in zip(*z[1])] for r in z[0]]]
-    for i in range(1,len(z)-1):
-        result.append([[sum(a*b for a,b in zip(r,c)) for c in zip(*result[i-1])] for r in z[i+1]])
-    return print(result[-1])
+    return print(reduce(lambda x,y : [[sum(a*b for a,b in zip(r,c)) for c in zip(*y)]for r in x], z))
 matrix_x=[[1,2,3],[4,5,6]]
 matrix_y=[[1,2],[3,4],[5,6]]
 #matrix_z=[[2,0],[0,2]]
