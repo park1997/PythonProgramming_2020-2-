29a0064420ec3a478c9d719dc3af9ca98a34d2b8
park1997 <iambyunghyun97@gmail.com>
2020-11-28 22:24:11 +0900

    python

--- ex01.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df_timeline=pd.read_excel("timeline.xlsx")
-print(df_timeline)
--- ex02.py
+++ ex02.py
@@ -1,23 +1,9 @@
-print("Welcome to the Course Manager v1.0")
-result=[]
+id_list=["asd"]
 while 1:
-    print()
-    print("Please input the # of the course : ",end="")
-    a=int(input())
-    if a>5:
-        print("Sorry, you have to input the # od courses, maximum is 5 only! ")
+    a=input("id")
+    if a in id_list:
+        print("이미 존재하는 아이디")
+        continue
     else:
-        while len(result)!=a :
-            print()
-            print("Please type the course name :",end="")
-            result.append(input())
-    if len(result)==a:
+        print("성공")
         break
-if a<=5:
-    print("Course list : {}".format(result))
-    fname = open("course_list.txt", mode = "w")
-    for i in result:
-        fname.write(i)
-        fname.write("\n")
-    print("The course list is saved in course_list.txt")
-fname.close()
