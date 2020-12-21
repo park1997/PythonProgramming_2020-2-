6545dcb6b06f040fe8cc4e04968b3abe94b9308e
park1997 <iambyunghyun97@gmail.com>
2020-12-21 12:27:25 +0900

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,26 +1,34 @@
-from collections import deque
+#6
 import sys
-a=int(sys.stdin.readline())
-result=deque()
-real=deque()
-for i in range(a):
-    b=int(sys.stdin.readline())
-    x=0
-    y=len(result)-1
-    while x<=y:
-        mid=(x+y)//2
-        if result[mid]<b:
-            x=mid+1
-        else:
-            y=mid-1
-    if x>=len(result):
-        result.append(b)
-    else:
-        result.insert(x,b)
+a=list(map(int,sys.stdin.readline().split()))
+b=list(map(int,sys.stdin.readline().split()))
+b.sort()
+print(b[a[1]-1])
 
-    if len(result)%2==0:
-        real.append(result[((len(result)+1)//2)-1])
+#5
+from math import gcd
+import sys
+a,b=map(int,sys.stdin.readline().split())
+print(gcd(a,b))
+
+ # 3
+ a={0:0,1:1,2:1}
+def f(n):
+    if n in a:
+        return a[n]
     else:
-        real.append(result[((len(result)+1)//2)-1])
-for i in real:
-    print(i)
+        a[n] = f(n-2) + f(n-1)
+        return a[n]
+print(f(int(input())))
+
+
+
+
+import sys
+def f(x):
+    num=set(range(2,x+1))
+    for i in range(2,x+1):
+        if i in num:
+            num-=set(range(2*i,x+1,i))
+    return len(num)
+print(f(int(sys.stdin.readline())))
