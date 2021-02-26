4e913c56a4127235c0dc3df5e6b7df110a003709
park1997 <iambyunghyun97@gmail.com>
2021-02-26 16:02:47 +0900

    Update p_chapter03_03.py

--- 파이썬중급/p_chapter03_03.py
+++ 파이썬중급/p_chapter03_03.py
@@ -18,7 +18,7 @@ print()
 from collections import  namedtuple
 Point = namedtuple("Point","x y")
 pt3 = Point(1.0, 5.0) # 클래스 형식으로 튜플을 추상화함, 키가 x ,y  value 가 1.0, 5.0 , 인덱스로도 접근은 가능함
-pt4 = Point(2.5,1.5)
+pt4 = Point(2.5, 1.5)
 print("2. ")
 print(pt3)
 print(pt4) # 튜풀형태로 데이터가 어떻게 들어있는지 확인가능
@@ -27,6 +27,74 @@ l_leng2 = sqrt((pt3.x-pt4.x)**2 + (pt3.y-pt4.y)**2)
 print(l_leng2)
 
 # 네임트튜플 선언 방법
+Point1 = namedtuple('Point', ["x","y"])
+Point2 = namedtuple("Point", 'x y')
+Point3 = namedtuple("Point", 'x,y')
+Point4 = namedtuple("Point","x y x class",rename=True) # Default = False
+
+# 튜플을 사용하는데 클래스로 나옴
+print()
+print("3. ")
+print(Point1, Point2, Point3, Point4)
+
+# Dict to unpacking
+temp_dict = {"x":75,"y":55}
+
+
+# 객체 생성
+p1 = Point1(10,35)
+p2 = Point2(20,40)
+p3 = Point3(45,20)
+p4 = Point4(10,20,30,40)
+p5 = Point3(**temp_dict) # 튜플은 언패킹 할떄 *한개 딕셔너리는 **두개
+
+
+print()
+print("4. ")
+print(p1)
+print(p2)
+print(p3)
+# rename test
+print(p4)
+print(p5)
+print()
+
+# 사용
+print(p1[0],p2[1])
+print(p1.x+p2.y) # 이런식으로 쓰는게 더 직관적임
+print()
+
+# unpacking
+print("5. ")
+x,y = p3
+print(x,y)
+
+# 네임드 튜플 메소드
+print()
+print("6.")
+temp = [52,38]
+# 리스트를 네임드튜플로 변환
+# ._make() :  새로운 객체 생성
+p4 = Point1._make(temp)
+print(p4)
+
+# _fields :  필드 네임 확인
+print()
+print("7. ")
+print(p1._fields, p2._fields, p3._fields, p4._fields)
+
+# ._asdict() : OrderDict 반환
+print()
+print("8. ")
+print(p1._asdict)
+print(p4._asdict)
+
+# 실 사용 실습
+# 반에 20 명있음 4개의 반(A,B,C,D)이있음
+
+
+
+
 
 
 
