9c04e26ef1a59dc4df58d1ecf3ba6f147662c940
park1997 <iambyunghyun97@gmail.com>
2021-08-07 11:59:21 +0900

    Create p_chapter04_02.py

---
+++ 파이썬중급/p_chapter04_02.py
@@ -0,0 +1,13 @@
+# chapter04_02
+# 시퀀스형
+# 컨테이너(container : 서로다른 자료형[list, tuple, collections.deque])
+# 플랫(Flat : 한개의 자료형[str, bytes, bytearray, array.array, memoryview])
+# 가변(list, bytearray, array, memoryview, deque)
+# 불변(tuple, str, bytes)
+# 리스트 및 튜플 고급
+
+# Tuple Advanced
+# Unpacking
+
+b ,a = a, b
+print(divmod(100,9))
