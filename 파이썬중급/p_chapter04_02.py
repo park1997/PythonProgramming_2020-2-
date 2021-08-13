c97ab1588a2bf287aba78059883874e7047e2479
park1997 <iambyunghyun97@gmail.com>
2021-08-13 17:19:01 +0900

    Update p_chapter04_02.py

--- 파이썬중급/p_chapter04_02.py
+++ 파이썬중급/p_chapter04_02.py
@@ -43,5 +43,23 @@ print()
 # reverse, key = len, key = str.Lower, key=func
 
 # sorted : 정렬 후 새로운 객체 반환
-# sort : 정렬 후 객체 직접 변경
+f_list =['orange','apple','papaya','lemon','strawberry','coconut']
+print('sorted - ',sorted(f_list))
+print('sorted(reverse) - ',sorted(f_list,reverse=True))
+print('sorted(len) - ',sorted(f_list,key=len))
+print('sorted(lambda) - ',sorted(f_list,key=lambda x:x[-1]))    # 끝 글자 기준 정렬
+print('sorted(lambda) - ',sorted(f_list,key=lambda x:x[-1],reverse=True))
+print()
 
+# sort : 정렬 후 객체 직접 변경
+# 반환 값 확인(None)
+print('sort - ',f_list.sort(),f_list)
+print('sort(reverse) - ',f_list.sort(reverse=True),f_list)
+print('sort(len) - ',f_list.sort(key=len),f_list)
+print('sort(lambda) - ',f_list.sort(key=lambda x:x[-1]),f_list)
+print('sort(lambda) - ',f_list.sort(key=lambda x:x[-1],reverse=True),f_list)
+print()
+
+# List vs Array 적합 한 사용법 설명
+# 리스트 기반 : 융통성, 다양한 자료형, 범용적 사용
+# 숫자 기반 : 배열(리스트와 거의 호환)
